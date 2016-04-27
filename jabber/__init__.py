f35eeab3f00c2594c190a139170e0939d47a7fb1
Gennadii Altukhov <galt@redhat.com>
2016-04-27 21:17:57 +0200

    Apply patch: Initial import of jabberpy Author: Michael Stahnke <stahnma@fedoraproject.org>

--- jabber/__init__.py
+++ jabber/__init__.py
@@ -1,2 +1,10 @@
-#!/usr/bin/python
-from jabber import *
+__all__ = []
+
+import jabber
+
+for __s in dir(jabber):
+    __val = getattr(jabber, __s)
+    globals()[__s] = __val
+    __all__.append(__val)
+
+del __s, __val
